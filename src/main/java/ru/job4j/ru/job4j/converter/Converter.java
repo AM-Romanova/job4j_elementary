2f4034e07e00e357bebe66aedcdbe40d28319bd9
Анна-Мария <anna.ma.romanova@gmail.com>
2024-01-02 00:51:33 +0300

    5. Конвертер валюты [#41583 #157122]

---
+++ src/main/java/ru/job4j/ru/job4j/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j.ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = value / 60;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(1000);
+        System.out.println(euro + " euros");
+        float dollar = Converter.rubleToDollar(1000);
+        System.out.println(dollar + " dollars");
+    }
+}
