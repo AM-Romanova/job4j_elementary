b8a264923d0c8deaed2cc4e9ce0555fe79ad9520
Анна-Мария <anna.ma.romanova@gmail.com>
2024-01-01 23:39:29 +0300

    4. Методы [#231984 #157120]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,18 +1,15 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
+    public static void plus(int first, int second, int third) {
+        int result = first + second + third;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four - two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        Calculator.plus(100, 500, 400);
+        Calculator.plus(4, 2, 14);
+        Calculator.plus(3, 5, 12);
     }
 }
