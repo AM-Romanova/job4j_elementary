20a2f3c42f4d3fc5d790522aa445ced9956a5258
Анна-Мария <anna.ma.romanova@gmail.com>
2024-01-11 05:05:21 +0300

    6.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/loop/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j.loop;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int result = i;
+            System.out.println(result);
+        }
+    }
+}
