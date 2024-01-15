9261fbbfe30e4704a864491d0ac9ed885e7df8a9
Анна-Мария <anna.ma.romanova@gmail.com>
2024-01-15 22:42:55 +0300

    My Calculator

---
+++ src/main/java/ru/job4j/calculator/MyCalculator.java
@@ -0,0 +1,47 @@
+package ru.job4j.calculator;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class MyCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = scanner.nextInt();
+
+        int result;
+
+        System.out.print("Введите действие: ");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
+
+        switch (action) {
+            case "+":
+                result = num1 + num2;
+                System.out.print("Результат: " + result);
+                break;
+            case "-":
+                result = num1 - num2;
+                System.out.print("Результат: " + result);
+                break;
+            case "*":
+                result = num1 * num2;
+                System.out.print("Результат: " + result);
+                break;
+            case "/":
+                if (num2 == 0)
+                    System.out.print("Error");
+                else {
+                    result = num1 / num2;
+                    System.out.print("Результат: " + result);
+                }
+                break;
+            default:
+                System.out.print("Неверные данные");
+        }
+    }
+}
