0670fd308904a3e33bbe44abace58c6adcd48ac1
Анна-Мария <anna.ma.romanova@gmail.com>
2024-02-11 03:38:55 +0300

    6.0.3. Заполнение массива. [#226300 #157098]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition1.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class ArrayDefinition1 {
+    public static void main(String[] args) {
+        String[] names = new String[4];
+                names[0] = "Jonh";
+                names[1] = "Anna";
+                names[2] = "Sara";
+                names[3] = "Jonny";
+
+        System.out.println("name is " + names[0]);
+        System.out.println("name is " + names[1]);
+        System.out.println("name is " + names[2]);
+        System.out.println("name is " + names[3]);
+    }
+}
