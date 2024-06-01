c90b1b399b95d98b0b58f7362e9d4ebdf734ef61
Анна-Мария <anna.ma.romanova@gmail.com>
2024-06-01 20:45:54 +0300

    1.5. Поля объекта [#166083 #157226]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,11 +2,27 @@ package ru.job4j.oop;
 
 public class Cat {
     public static void main(String[] args) {
-        Cat pappy = new Cat();
-        Cat sparky = new Cat();
-        String say = pappy.sound();
-        System.out.println("Pappy say " + say);
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        cat1.giveNick("Гав");
+        cat2.giveNick("Блэк");
+        cat1.show("Котлета");
+        cat2.show("Рыба");
     }
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+        System.out.println(this.name);
+    }
+
+    private String food;
+
+    public void show(String eat) {
+        this.food = eat;
+        System.out.println(this.food);
+    }
+
     public String sound() {
         String voice = "мяу-мяу";
         return voice;
