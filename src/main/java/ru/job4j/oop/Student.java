8a63a5563c351b05743967a3e4ce6672e4491fc8
Анна-Мария <anna.ma.romanova@gmail.com>
2024-06-01 19:42:26 +0300

    1.2. Вызов метода объекта [#166085 #157225]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Student {
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.sing();
+    }
+
+    public void music() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Tra tra tra");
+        }
+    }
+
+    public void sing() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("I believe I can fly");
+        }
+    }
+}
